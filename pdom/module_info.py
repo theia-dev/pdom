621fb2a2b06643a9a76ae7214c30694e0263671c
Hagen Eckert <hagen.eckert@nadaro.net>
2020-06-24 12:43:18 +0200

    specifically include pdom/data/__init__.py in PyPI builds

--- pdom/module_info.py
+++ pdom/module_info.py
@@ -1,6 +1,6 @@
 app_name = 'pdom'
 app_group_id = 'net.nadaro.theia'
-version_number = (0, 0, 6)
+version_number = (0, 0, 7)
 version = f'{version_number[0]}.{version_number[1]}.{version_number[2]}'
 app_author = 'Hagen Eckert'
 url = 'https://pdom.rtfd.io/'
